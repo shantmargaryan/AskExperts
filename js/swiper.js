d698c6ee9fb0d0c8cbf54cdceeb8be4b04e27389
Shant <shant212205@gmail.com>
2024-06-04 10:09:48 -0800

    update

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,7 @@
 import Swiper from "swiper";
-import { Navigation } from 'swiper/modules';
-import "swiper/css"
-import 'swiper/css/navigation';
+import { Navigation } from '/swiper/modules';
+import "/swiper/css"
+import '/swiper/css/navigation';
 
 
 const feedbackSwiper = new Swiper('.feedback__swiper', {

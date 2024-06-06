1fe1404cd848409e000081d45ecd0f1c7b1943a1
Shant <shant212205@gmail.com>
2024-06-06 07:44:48 -0800

    update

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,20 @@
 const feedbackSwiper = new Swiper('.feedback__swiper', {
     loop: true,
-    spaceBetween: 30,
+    spsaceBetween: 30,
 
     // Navigation arrows
-    modules: [Navigation],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 });
 
+
 const businessSwiper = new Swiper('.business__swiper', {
     loop: true,
     spaceBetween: 30,
 
     // Navigation arrows
-    modules: [Navigation],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
